e007143692915167d274127f7e636bdafe3fbc36
alenjoby <alenjoby955@gmail.com>
2023-10-01 12:02:25 +0400

    Excersice  4

---
+++ Chapter 1- Getting Started with Python/Exercises/Excersice4.py
@@ -0,0 +1,14 @@
+a="alen"
+b="joby"
+c="Creative Computing"
+
+str=a+b+c
+#with the plus operator
+total = a,b,c
+#without the Plus operator
+
+print(str)
+#output for plus operator
+
+print(total)
+# output for non plus operator
